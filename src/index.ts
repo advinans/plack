0fa2f57ea27784b7513dc01eed00276f6f329cd5
Victor Andrée <victor.andree@advinans.se>
2018-09-06 18:32:33 +0200

    chore: update to pino 5

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ export {
 import { LogEntry } from './constants';
 import path = require('path');
 import { Writable, Duplex, Transform } from 'stream';
+import { asJson } from './util';
+
+const { asJsonSym, endSym, lsCacheSym } = require('pino/lib/symbols');
 
 export interface LogFn {
   (msg: string, ...args: any[]): void;
@@ -55,83 +58,18 @@ const severity: any = {
   10: 'DEBUG',
   20: 'DEBUG',
   30: 'INFO',
+  35: 'NOTICE',
   40: 'WARNING',
   50: 'ERROR',
   60: 'CRITICAL',
+  70: 'ALERT',
+  80: 'EMERGENCY',
 };
 
 function addLevel(this: any, name: string, lvl: number) {
   // all we do is fix the lscache
   Object.getPrototypeOf(this).addLevel(name, lvl);
   (this as any)._lscache[lvl] = flatstr(`{"severity":"${name.toUpperCase()}"`);
-}
-
-// Need to override this to change how errors are serialized
-// Copy-paste
-function asJson(
-  this: any,
-  obj: any,
-  msg: string | undefined | Error,
-  num: number,
-) {
-  // to catch both null and undefined
-  var hasObj = obj !== undefined && obj !== null;
-  var objError = hasObj && obj instanceof Error;
-  var msgErr = msg instanceof Error;
-  var err = undefined;
-  if (objError) {
-    err = obj;
-  } else if (msgErr) {
-    err = msg;
-  }
-  var msgIsStackTrace = msgErr || (!msg && objError);
-
-  msg = msgIsStackTrace ? err.stack : msg || undefined;
-  var data = this._lscache[num] + this.time();
-  if (msg !== undefined) {
-    // JSON.stringify is safe here
-    data += this.messageKeyString + JSON.stringify('' + msg);
-  }
-  // we need the child bindings added to the output first so that logged
-  // objects can take precedence when JSON.parse-ing the resulting log line
-  data = data + this.chindings;
-  var value;
-  if (hasObj) {
-    var notHasOwnProperty = obj.hasOwnProperty === undefined;
-    if (err) {
-      // CHANGE: Only print stack trace if it was not the message
-      data += ',"type":"' + err.constructor.name + '"';
-      if (!msgIsStackTrace) {
-        data += ',"stack":' + this.stringify(err.stack);
-      }
-
-      if (this.serviceContext && (msgIsStackTrace || obj.reportLocation)) {
-        data += ',"serviceContext":' + this.stringify(this.serviceContext);
-      }
-
-      if (!objError) {
-        const { name, ...rest } = err;
-        obj = { ...obj, ...rest };
-      }
-    }
-    for (var key in obj) {
-      if (objError && key === 'name') {
-        continue;
-      }
-
-      value = obj[key];
-      if (
-        (notHasOwnProperty || obj.hasOwnProperty(key)) &&
-        value !== undefined
-      ) {
-        value = this.stringify(value);
-        if (value !== undefined) {
-          data += ',"' + key + '":' + value;
-        }
-      }
-    }
-  }
-  return data + this.end;
 }
 
 export function plack(): Logger;
@@ -146,17 +84,25 @@ export function plack(
 ): Logger {
   options = options || {};
 
-  const instance = pino({
-    // dont include hostname, pid et cetera
-    base: options.base || {},
-    messageKey: 'message',
-    serializers: {
-      // err property is not serialized because it is special cased
-      err: () => undefined,
-      ...options.serializers,
-    },
-    ...options,
-  } as any, destination as any);
+  const instance = pino(
+    {
+      messageKey: 'message',
+      changeLevelName: 'severity',
+      useLevelLabels: true,
+      base: options.base || {},
+      serializers: {
+        err: () => undefined,
+        ...options.serializers,
+      },
+      customLevels: {
+        notice: 35,
+        alert: 70,
+        emergency: 80,
+      },
+      ...options,
+    } as any,
+    destination as any,
+  );
 
   // For error reporting
   (instance as any).serviceContext =
@@ -165,30 +111,15 @@ export function plack(
   // dont include log version specifier
   (instance as any).end = '}\n';
 
-  // override lscache to output severity and not level
-  Object.defineProperty(instance, '_lscache', {
-    value: Object.keys(severity).reduce(
-      (o, k) => {
-        o[k] = flatstr(`{"severity":"${severity[k]}"`);
-        return o;
-      },
-      {} as any,
-    ),
-  });
+  instance[lsCacheSym] = Object.keys(instance.levels.labels).reduce((o, k) => {
+    (o as any)[k] = `{"severity":"${severity[Number(k)]}"`;
+    return o;
+  }, instance[lsCacheSym]);
 
-  Object.defineProperty(instance, 'addLevel', {
-    value: addLevel,
-  });
-
-  Object.defineProperty(instance, 'asJson', {
-    value: asJson,
-  });
+  (instance as any)[endSym] = '}\n';
+  (instance as any)[asJsonSym] = asJson;
 
   instance.critical = instance.fatal;
-
-  instance.addLevel('notice', 35);
-  instance.addLevel('alert', 70);
-  instance.addLevel('emergency', 80);
   return (instance as any) as Logger;
 }
 
